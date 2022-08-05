1fe4e728a7fa935541f157cf769356e995a06a53
vengatesanns <vengat19amd@gmail.com>
2022-08-05 09:27:19 +0530

    CrossOrigin Enabled and YAML updated

--- restaurant-system/restaurant-service/src/main/java/com/hackprotech/restaurantservice/controller/RestaurantController.java
+++ restaurant-system/restaurant-service/src/main/java/com/hackprotech/restaurantservice/controller/RestaurantController.java
@@ -3,6 +3,7 @@ package com.hackprotech.restaurantservice.controller;
 import com.hackprotech.restaurantservice.RestaurantService;
 import com.hackprotech.restaurantservice.models.RestaurantEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/restaurant")
+@CrossOrigin("*")
 public class RestaurantController {
 
     @Autowired
@@ -19,18 +21,18 @@ public class RestaurantController {
     @GetMapping("/fetch/{restaurantId}")
     public ResponseEntity<RestaurantEntity> fetchRestaurantDetails(@PathVariable long restaurantId) {
         RestaurantEntity restaurantEntity = restaurantService.getRestaurantDetails(restaurantId);
-        return ResponseEntity.status(200).body(restaurantEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(restaurantEntity);
     }
 
     @GetMapping("/fetch-all")
     public ResponseEntity<List<RestaurantEntity>> fetchAllRestaurantDetails() {
-        return ResponseEntity.status(200).body(restaurantService.getAllRestaurantDetails());
+        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.getAllRestaurantDetails());
     }
 
     @PostMapping("/save")
     public ResponseEntity<RestaurantEntity> saveRestaurantDetails(@Valid @RequestBody RestaurantEntity restaurantRequest) {
         RestaurantEntity restaurantEntity = restaurantService.saveRestaurantDetails(restaurantRequest);
-        return ResponseEntity.status(200).body(restaurantEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(restaurantEntity);
     }
 
 }
