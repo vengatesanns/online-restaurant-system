08636179b6368c86e04df0d396a61762774e4a5e
vengatesanns <vengat19amd@gmail.com>
2022-08-27 08:34:23 +0530

    Request Updated

--- restaurant-system/order-service/src/main/java/com/hackprotech/orderservice/request/FoodItemsRequest.java
+++ restaurant-system/order-service/src/main/java/com/hackprotech/orderservice/request/FoodItemsRequest.java
@@ -1,10 +1,12 @@
 package com.hackprotech.orderservice.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class FoodItemsRequest {
     private Long foodId;
     private Integer quantity;
